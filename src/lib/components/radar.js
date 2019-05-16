082959de9707596865f813154976e254fad53822
Jacek Dominiak <jacekdominiak@outlook.com>
2019-05-16 10:32:15 +0200

    removing the changes, shapeProps are enough

--- src/lib/components/radar.js
+++ src/lib/components/radar.js
@@ -65,20 +65,6 @@ const shape = (columns, options) => (chartData, i) => {
   const data = chartData.data;
   const meta = chartData.meta || {};
   const extraProps = options.shapeProps(meta);
-  let extraPropsSvg = {};
-  if (!meta.fill) {
-    meta.fill = meta.color;
-  }
-  console.log("meta", meta, "extraProps", extraProps);
-  if (meta.strokeWidth) {
-    extraPropsSvg.strokeWidth = meta.strokeWidth;
-  }
-  if (meta.strokeDasharray) {
-    extraPropsSvg.strokeDasharray = meta.strokeDasharray;
-  }
-  if (meta.stokeLinecap) {
-    extraPropsSvg.stokeLinecap = meta.stokeLinecap;
-  }
   return (
     <path
       key={`shape-${i}`}
@@ -93,7 +79,6 @@ const shape = (columns, options) => (chartData, i) => {
         })
       )}
       {...extraProps}
-      {...extraPropsSvg}
       stroke={meta.color}
       fill={meta.fill}
       className={[extraProps.className, meta.class].join(" ")}
